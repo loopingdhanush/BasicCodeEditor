ca786d0f0727c3e8e40fbca7d36d3a41d57de7f7
Dhanush S <dhanush.utility@gmail.com>
2025-02-03 23:16:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -7,15 +7,27 @@ function run(){
     output.contentDocument.body.innerHTML = htmlCode.value +"<style>"+cssCode.value+"</style>";
     output.contentWindow.eval(jsCode.value)
 }
-
 function dlmode(){
     if(document.body.style.backgroundColor==="white"){
         document.body.style.backgroundColor="";
         document.body.style.color="";
         document.getElementById("dlbut").style.color="";
+        document.getElementById("html-code").style.backgroundColor="";
+    document.getElementById("css-code").style.backgroundColor="";
+    document.getElementById("js-code").style.backgroundColor="";
+    document.getElementById("html-code").style.outline="";
+    document.getElementById("css-code").style.outline="";
+    document.getElementById("js-code").style.outline="";
     } else {
     document.body.style.backgroundColor="white";
     document.body.style.color="black";
     document.getElementById("dlbut").style.color="black";
+    document.getElementById("html-code").style.backgroundColor="white";
+    document.getElementById("css-code").style.backgroundColor="white";
+    document.getElementById("js-code").style.backgroundColor="white";
+    document.getElementById("html-code").style.outline="2px solid black";
+    document.getElementById("css-code").style.outline="2px solid black";
+    document.getElementById("js-code").style.outline="2px solid black";
+    document.getElementById("output").style.outline="2px solid black";
     }
 }
